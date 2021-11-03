04f6ce7d49889c7d8cd59a54845ef16b19f068c0
Ian <flyingiancheng@gmail.com>
2021-11-03 21:43:25 +0800

    add to upload

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,5 +1,7 @@
+from decouple import config
+import dj_database_url
+import django_heroku
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -70,11 +72,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': dj_database_url.config()
 }
+
 
 
 # Password validation
@@ -114,3 +114,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
